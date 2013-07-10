8fed9e0bc0220237dde1b5b7fbd1936b9efacf7b
Toson <toson777@gmail.com>
2013-07-09 21:14:44 -0700

    create resource

---
+++ Resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SummerPractice.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SUMMERPRACTICE_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SUMMERPRACTICE              107
+#define IDI_SMALL                       108
+#define IDC_SUMMERPRACTICE              109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDD_DIALOG3                     131
+#define IDD_DIALOG2                     132
+#define IDD_DIALOG4                     133
+#define IDD_DIALOG5                     134
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON2                     1006
+#define IDC_BUTTON1                     1007
+#define IDC_BUTTON3                     1011
+#define IDC_BUTTON4                     1012
+#define IDC_BUTTON5                     1013
+#define IDC_BUTTON6                     1014
+#define IDC_BUTTON7                     1015
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
